96fc5281f45b844be990670527a075dc8fee84a6
unknown <yavor.d0@gmail.com>
2019-09-17 17:39:40 +0300

    .

---
+++ lib/models/med_tile.dart
@@ -0,0 +1,15 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+
+//@immutable
+//class MedTile extends Equatable {
+//  final bool complete;
+//  final String name;
+//  final String dose;
+//  final String form;
+//  final String doses;
+//  final String schedule;
+//
+//  MedTile()
+//}
