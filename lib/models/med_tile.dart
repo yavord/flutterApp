5fc64b5e5bd5fdc8dff59c000b4a4dc38b4b9b7d
unknown <yavor.d0@gmail.com>
2019-09-18 14:52:51 +0300

    .

--- lib/models/med_tile.dart
+++ lib/models/med_tile.dart
@@ -2,14 +2,34 @@ import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 
-//@immutable
-//class MedTile extends Equatable {
-//  final bool complete;
-//  final String name;
-//  final String dose;
-//  final String form;
-//  final String doses;
-//  final String schedule;
-//
-//  MedTile()
-//}
+@immutable
+class MedTile extends Equatable {
+  final bool complete;
+  final String name;
+  final String dose;
+  final String form;
+  final String doses;
+  final String schedule;
+
+  //TODO: Edit constructors to fit database input/output
+  MedTile({
+    this.complete = false,
+    @required this.name,
+    this.dose,
+    this.form,
+    this.doses,
+    this.schedule
+  }) : super([complete, name, dose, form, doses, schedule]);
+
+  //TODO: implement MedTileEntity
+//  MedTileEntity toEntity() {
+//    return MedTileEntity(name, dose, form, doses, schedule);
+//  }
+
+  //TODO: implement this thing
+//  static MedTile fromEntity(MedTileEntity entity) {
+//    return MedTile(
+//      entity.
+//    )
+//  }
+}
