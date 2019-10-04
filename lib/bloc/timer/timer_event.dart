beab6c28123039b7fa4a9a186d5b9dbc777ab00d
Yavor Dimitrov <yavor.d0@gmail.com>
2019-10-04 15:30:03 +0300

    circle timer events

--- lib/bloc/timer/timer_event.dart
+++ lib/bloc/timer/timer_event.dart
@@ -1,8 +1,15 @@
 import 'package:equatable/equatable.dart';
+import 'package:hmss/models/models.dart';
 import 'package:meta/meta.dart';
 
 
 @immutable
 abstract class CircleTimerEvent extends Equatable {
   CircleTimerEvent([List props = const[]]) : super(props);
+}
+
+class UpdateCircleTimer extends Equatable {
+  final MedTile medTile;
+
+  UpdateCircleTimer(this.medTile) : super([medTile]);
 }
