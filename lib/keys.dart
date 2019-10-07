b40c0aa15a166271a7fa0320c5b0d522f43ec844
Yavor Dimitrov <yavor.d0@gmail.com>
2019-10-07 17:23:35 +0300

    .

--- lib/keys.dart
+++ lib/keys.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/foundation.dart';
 
 class TherapyKeys {
+  //Screens
   static final home = const Key('__home__');
 
+  //Med Tiles
   static final medTileItem = (String id) => Key('MedTileItem__${id}');
+  static final medTileItemName = (String id) => Key('MedTileItem__${id}__Name');
+  static final medTileItemDose = (String id) => Key('MedTileItem__${id}__Dose');
+  static final medTileItemForm = (String id) => Key('MedTileItem__${id}__Form');
+  static final medTileItemDoses = (String id) => Key('MedTileItem__${id}__Doses');
+  static final medTileItemSchedule = (String id) => Key('MedTileItem__${id}__Schedule');
 
+  //Tabs
   static final tabs = const Key('__tabs__');
   static final medIdTab = const Key('__medId');
 
--- lib/widgets/medication_tiles.dart
+++ lib/widgets/medication_tiles.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'package:hmss/keys.dart';
 import 'package:hmss/models/models.dart';
 
 
@@ -23,7 +25,41 @@ class MedTileItem extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5.0),
       child: Card(
-        
+        elevation: 0,
+         color: Theme.of(context).cardColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(11),
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      new Container(
+                        child: new Icon(
+                          FontAwesomeIcons.pills,
+                          size: 20,
+                        ),
+                      ),
+                      SizedBox(width: 13),
+                      Text(medTile.name, 
+                      key: TherapyKeys.medTileItemName(medTile.id),
+                      style: TextStyle(fontSize: 22,),
+                      ) 
+                    ],
+                    )
+                ],
+                ),
+            ),
+          ],
+        ),
       ),
     );
   }
