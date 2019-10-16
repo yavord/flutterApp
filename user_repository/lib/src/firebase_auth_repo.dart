b38f4789a4af8e8c9bfccfe76a9dfdc30356003b
Yavor Dimitrov <yavor.d0@gmail.com>
2019-10-16 16:48:23 +0300

    auth with login

--- user_repository/lib/src/firebase_auth_repo.dart
+++ user_repository/lib/src/firebase_auth_repo.dart
@@ -1,12 +1,27 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:user_repository/auth_repo.dart';
 
 
 class FireBaseAuthRepo implements AuthRepo {
   final FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
 
-  FireBaseAuthRepo ({FirebaseAuth firebaseAuth})
-    : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+  FireBaseAuthRepo ({FirebaseAuth firebaseAuth, GoogleSignIn googleSignin})
+    : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
+      _googleSignIn = googleSignin ?? GoogleSignIn();
+
+  Future<FirebaseUser> signInWithGoogle() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    await _firebaseAuth.signInWithCredential(credential);
+    return _firebaseAuth.currentUser();
+  }
 
   Future<bool> isAuthenticated() async {
     final currentUser = await _firebaseAuth.currentUser();
