2e81e0f1c560765e5f79bc4136165901c31e2fc3
Yavor Dimitrov <yavor.d0@gmail.com>
2019-11-01 20:06:20 +0200

    .

--- lib/widgets/medtile/circle_timer_new.dart
+++ lib/widgets/medtile/circle_timer_new.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 import 'package:proba123/bloc/timer/timer.dart';
 import 'package:proba123/util/ticker.dart';
 import 'package:proba123/util/timer_painter.dart';
+import 'package:proba123/util/const.dart';
+import 'package:proba123/localization.dart';
 
 
   // var string = '8:30';
@@ -26,12 +29,41 @@ class CircleTimer extends StatelessWidget {
             child: Stack(
               children: <Widget>[
                 Positioned.fill(
-                  child: BlocBuilder<TimerBloc, TimerState>(
-                    builder: (context, state) {
-                      
-                    },
+                  child: null,
+                ),
+                Align(
+                  alignment: FractionalOffset.center,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          AppLocalizations().takeAfter,
+                        ),
+                        BlocBuilder<TimerBloc, TimerState>(
+                          builder: (context, state) {
+                            final String hourStr = ((state.nextIntake / 60) % 60)
+                              .floor()
+                              .toString()
+                              .padLeft(2,'0');
+                            final String minStr = (state.nextIntake % 60)
+                              .floor()
+                              .toString()
+                              .padLeft(2,'0');
+                            return new AutoSizeText(
+                                '$hourStr:$minStr',
+                                maxLines: 1,
+                                stepGranularity: 8,
+                                minFontSize: 16,
+                                style: TextStyle(fontSize: 24.0));
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                )
+                ),
               ],
             ),
           ),
