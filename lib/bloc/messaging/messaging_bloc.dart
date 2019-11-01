83f6dbe41da22e75c504c70bd06625756f4cbf4f
Yavor Dimitrov <yavor.d0@gmail.com>
2019-11-01 13:00:22 +0200

    token saving

--- lib/bloc/messaging/messaging_bloc.dart
+++ lib/bloc/messaging/messaging_bloc.dart
@@ -39,10 +39,11 @@ class MessagingBloc extends Bloc<MessagingEvent, MessagingState> {
 
   Stream<MessagingState> _mapMessagingInitToState() async* {
     try{
-      final token = await _messagingRepo.getToken();
-      final user = await _authRepo.getUser();
+      // final token = await _messagingRepo.getToken();
+      // final user = await _authRepo.getUser();
+      await _messagingRepo.getToken();
       await _messagingRepo.setConfig();
-      await _firestoreRepo.addToken(TokenEntity(token, user));
+      // await _firestoreRepo.addToken(TokenEntity(token, user));
       yield MessagingInitlialized();
     } catch (e) {
       print(e);
