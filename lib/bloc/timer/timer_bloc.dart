b3c80abc090de974effe16e82551121751a1ae0e
Yavor Dimitrov <yavor.d0@gmail.com>
2019-11-01 11:12:52 +0200

    timerbloc/ticker

---
+++ lib/bloc/timer/timer_bloc.dart
@@ -0,0 +1,16 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:meta/meta.dart';
+
+import 'timer.dart';
+import 'package:proba123/util/ticker.dart';
+
+
+class TimerBloc extends Bloc<TimerEvent, TimerState> {
+  final double _nextIntake;
+  final Ticker _ticker;
+  final AnimationController _controller;
+  
+  
+}
