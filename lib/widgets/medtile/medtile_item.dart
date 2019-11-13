2b44d9eec48daeafae7069fb8b7a5a042df5a7af
Yavor Dimitrov <yavor.d0@gmail.com>
2019-11-13 16:08:33 +0200

    import

--- lib/widgets/medtile/medtile_item.dart
+++ lib/widgets/medtile/medtile_item.dart
@@ -2,7 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:intl/intl.dart';
 import 'package:proba123/bloc/timer/timer_event.dart';
 
 import 'package:proba123/keys.dart';
