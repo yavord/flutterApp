b3dd420e4cf37c130b7d544d7723f11706d35d2d
Yavor Dimitrov <yavor.d0@gmail.com>
2019-11-20 18:24:58 +0200

    medId model update

--- lib/models/med_id.dart
+++ lib/models/med_id.dart
@@ -4,18 +4,18 @@ import 'package:meta/meta.dart';
 
 @immutable
 class MedId extends Equatable {
-  final String id;
+  final int id;
   final String patientName;
-  final String doctorName;
-  final String doctorNumber;
+  final List<Map> patientInfo;
+  final List<Map> doctorInfo;
 
   MedId({
     @required this.id,
     @required this.patientName,
-    @required this.doctorName,
-    @required this.doctorNumber,
+    @required this.patientInfo,
+    @required this.doctorInfo,
   });
 
   @override
-  List<Object> get props => [id, patientName, doctorName, doctorNumber];
+  List<Object> get props => [id, patientName, patientInfo, doctorInfo];
 }
